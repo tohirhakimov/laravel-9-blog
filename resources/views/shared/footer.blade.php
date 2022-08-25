ab4ede2a918b6b26834a0c224be0c5e54f06b513
Tohir <hakimovtohirbek@gmail.com>
2022-08-26 03:06:54 +0500

    add seeder, factories

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -2,12 +2,11 @@
     <div class="container justify-content-center">
         <ul class="navbar-nav text-center">
             <li class="nav-item text-white m-3">
-                Made with <i class="fa fa-heart text-danger" aria-hidden="true"></i> by <a href="https://guillaumebriday.fr" target="_blank" class="text-secondary">Guillaume Briday</a>
+                Made with <i class="fa fa-heart text-danger" aria-hidden="true"></i> by <a href="https://github.com/tohirhakimov" target="_blank" class="text-secondary">Tohir Hakimov</a>
             </li>
 
             <li class="nav-item text-white m-3">
-                <a href="https://github.com/guillaumebriday/laravel-blog" target="_blank" class="btn btn-outline-secondary mt-1"><i class="fa fa-github" aria-hidden="true"></i> Fork me on GitHub</a>
-                <a href="https://twitter.com/guillaumebriday" target="_blank" class="btn btn-outline-secondary mt-1"><i class="fa fa-twitter" aria-hidden="true"></i> Say Hi on Twitter !</a>
+                <a href="https://github.com/tohirhakimov/laravel-9-blog" target="_blank" class="btn btn-outline-secondary mt-1"><i class="fa fa-github" aria-hidden="true"></i> Fork me on GitHub</a>
             </li>
 
             <li class="nav-item m-3">
